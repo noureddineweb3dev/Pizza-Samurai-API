85ba9bfcaae562ceab8af31869651d74bfd8d81a
NOUREDDINE <noureddine.web3dev@gmail.com>
2025-12-30 20:47:05 +0100

    Orders added to test the API

--- data/orders.js
+++ data/orders.js
@@ -1,1 +1,14 @@
-export const orders = [];
+export const orders = [
+  {
+    id: 'SX1234',
+    customer: 'Akira',
+    items: ['katana-margherita', 'samurai-pepperoni'],
+    totalPrice: 12.5,
+  },
+  {
+    id: 'QW5678',
+    customer: 'Yumi',
+    items: ['katana-margherita', 'samurai-cola'],
+    totalPrice: 10.5,
+  },
+];
