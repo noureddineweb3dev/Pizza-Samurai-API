19a2991e5ebd9557fdb0412756192b0f71c57b6e
NOUREDDINE <noureddine.web3dev@gmail.com>
2025-12-30 12:00:22 +0100

    Initial Pizza Samurai API

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import menuRoutes from './routes/menuRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/menu', menuRoutes);
+
+app.get('/', (req, res) => {
+  res.send('🍕 Pizza Samurai API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
